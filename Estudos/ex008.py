4c3dcd3e36f347e8b40b8edf1cea3bf9bb1ba88f
Pedr0Lucca <107141461+Pedr0Lucca@users.noreply.github.com>
2023-01-02 10:29:32 -0300

    ex008

---
+++ Estudos/ex008.py
@@ -0,0 +1,4 @@
+v = float(input('Digite um valor em metros para saber quantos cm e quantos milimetros ele tem: '))
+cm = v*100
+m = cm*10
+print('{} metros tem {} centímetros e {} milímetros'.format(v, cm, m))
