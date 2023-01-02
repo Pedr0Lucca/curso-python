5345dffd0bf21ce291a9cce139a4c082a190e746
Pedr0Lucca <107141461+Pedr0Lucca@users.noreply.github.com>
2023-01-02 11:20:02 -0300

    ex011

---
+++ Estudos/ex011.py
@@ -0,0 +1,4 @@
+L = float(input('Qual a largura da sua parede? '))
+A = float(input('Qual a altura da sua parede? '))
+A2 = ((L*A)/2)
+print('Você precisará de {} baldes de tinta para pintar a sua parede'.format(A2))
